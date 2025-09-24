4306403538c014e7621f01192b3180039bc14361
CyrilJl <joly.cyril7@gmail.com>
2025-09-24 21:15:02 +0200

    refactor: add type hints to geocode function

--- rasterizer/rasterizer.py
+++ rasterizer/rasterizer.py
@@ -1,7 +1,8 @@
 import rioxarray
+import xarray as xr
 
 
-def geocode(ds, x_name, y_name, crs):
+def geocode(ds: xr.Dataset, x_name: str, y_name: str, crs):
     ds.rio.set_spatial_dims(x_dim=x_name, y_dim=y_name, inplace=True)
     ds.rio.write_crs(crs, inplace=True)
     return ds
